e1f51482f84dd080eaa5c61b6f97acd2a5872e26
GKnight9 <63012852+GKnight9@users.noreply.github.com>
2021-12-12 15:30:10 -0700

    Add files via upload

---
+++ 2420_Labs/src/BSTNode.java
@@ -0,0 +1,18 @@
+package binarySearchTrees;
+/**
+ * 
+ * @author Garrett Knight
+ * CSIS 2420
+ * Weeks 9 - 10 Assignment #1
+ * This is the node class assigning the values used in the Binary Search Tree
+ */
+public class BSTNode {
+	
+	int data;
+	BSTNode left;
+	BSTNode right;
+
+	public BSTNode(int data) {
+		this.data = data;
+	}
+}
